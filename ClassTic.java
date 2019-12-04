3dbe6d988deb8457653ac886d9df82ea1ddd42c1
Dmitry Zhuravlev <sheriff_lainer@mail.ru>
2019-12-04 23:10:02 +0300

    Туц ашду шы фввув

---
+++ ClassTic.java
@@ -0,0 +1,112 @@
+/**
+ * Java 1. Lesson 4. Tic-tac-toe in console
+ * written Aug 4, 2017
+ */
+import java.util.*;
+
+class ClassTic {
+
+    final int SIZE = 3;
+    final char DOT_X = 'x';
+    final char DOT_O = 'o';
+    final char DOT_EMPTY = '.';
+    char[][] map = new char[SIZE][SIZE];
+    Scanner sc = new Scanner(System.in);
+    Random rand = new Random();
+
+    public static void main(String[] args) {
+        new ClassTic();
+    }
+
+    ClassTic() {
+        initMap();
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("YOU WON!");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("AI WON!");
+                break;
+            }
+            /*if (isMapFull()) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }*/
+        }
+        System.out.println("GAME OVER.");
+    }
+
+    void initMap() {
+        for (int i = 0; i < SIZE; i++)
+            for (int j = 0; j < SIZE; j++)
+                map[i][j] = DOT_EMPTY;
+    }
+
+    void printMap() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++)
+                System.out.print(map[i][j] + " ");
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Enter X and Y (1..3):");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y));
+        map[y][x] = DOT_X;
+    }
+
+    void aiTurn() {
+        int x, y;
+        do {
+            x = rand.nextInt(SIZE);
+            y = rand.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        map[y][x] = DOT_O;
+    }
+
+    boolean checkWin(char dot) {
+        // check horizontals
+        if (map[0][0] == dot && map[0][1] == dot && map[0][2] == dot) return true;
+        if (map[1][0] == dot && map[1][1] == dot && map[1][2] == dot) return true;
+        if (map[2][0] == dot && map[2][1] == dot && map[2][2] == dot) return true;
+        // check verticals
+        if (map[0][0] == dot && map[1][0] == dot && map[2][0] == dot) return true;
+        if (map[0][1] == dot && map[1][1] == dot && map[2][1] == dot) return true;
+        if (map[0][2] == dot && map[1][2] == dot && map[2][2] == dot) return true;
+        // check diagonals
+        if (map[0][0] == dot && map[1][1] == dot && map[2][2] == dot) return true;
+        if (map[2][0] == dot && map[1][1] == dot && map[0][2] == dot) return true;
+        return false;
+    }
+
+    boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++)
+            for (int j = 0; j < SIZE; j++)
+                if (map[i][j] == DOT_EMPTY)
+                    return false;
+        return true;
+    }
+
+    boolean isCellValid(int x, int y) {
+        if (x < 0 || y < 0 || x >= SIZE || y >= SIZE)
+            return false;
+        if (map[y][x] == DOT_EMPTY)
+            return true;
+        return false;
+    }
+}
